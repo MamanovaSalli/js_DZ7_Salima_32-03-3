b273719992448769b8a06a31ed78e09e651bfc60
Salima <mamanovasalima441@gmail.com>
2023-12-13 00:16:11 +0300

    dz2

--- js/home_works.js
+++ js/home_works.js
@@ -1,29 +1,114 @@
-
 const checkGmail = document.querySelector('#gmail_input')
 const gmailButt = document.querySelector('#gmail_button')
 const gmailRes = document.querySelector('#gmail_result')
 
-const regExp =  /^\w+@+[gmail]\w+\.+[com]+$/i
+const regExp = /^\w+@+[gmail]\w+\.+[com]+$/i
 
-gmailButt.onclick= ()=>{
-    if (regExp.test(checkGmail.value)){
-        gmailRes.innerHTML='OK'
+gmailButt.onclick = () => {
+    if (regExp.test(checkGmail.value)) {
+        gmailRes.innerHTML = 'OK'
         gmailRes.style.color = 'blue'
-    }else {
-        gmailRes.innerHTML='NOT OK'
+    } else {
+        gmailRes.innerHTML = 'NOT OK'
         gmailRes.style.color = 'red'
     }
 }
 
-let position = 0;
-function recursionAnimation() {
-    position = position + 7;
-    if (position>450) return;
+const childBlock = document.querySelector('.child_block')
 
-    document.querySelector('.child_block').style.left = position + 'px';
-    animation();
+const moveSpeedChildBlock = 0.5
+const parentBlockWith = 450
+
+let positionX = 0
+let positionY = 0
+
+const moveBlock = () => {
+    if (positionY < parentBlockWith && positionX === 0) {
+        positionY++
+        childBlock.style.top = `${positionY}px`
+        setTimeout(moveBlock, moveSpeedChildBlock)
+    } else if (positionY >= parentBlockWith && positionX < parentBlockWith) {
+        positionX++
+        childBlock.style.left = `${positionX}px`
+        setTimeout(moveBlock, moveSpeedChildBlock)
+    } else if (positionY > 0 && positionX >= parentBlockWith) {
+        positionY--
+        childBlock.style.top = `${positionY}px`;
+        setTimeout(moveBlock, moveSpeedChildBlock)
+    } else if (positionY === 0 && positionX > 0) {
+        positionX--
+        childBlock.style.left = `${positionX}px`;
+        setTimeout(moveBlock, moveSpeedChildBlock)
+    }
 }
-function animation() {
-    setTimeout(recursionAnimation, 50);
-}
-animation();
+moveBlock();
+
+
+// STOP WATCH
+
+const watchBlock = document.querySelector('.intervalMinutes')
+const watchSecond = document.querySelector('.intervalSecond')
+const watchHundSecond = document.querySelector('.intervalHundredths-second')
+const watchBlockStart = document.querySelector('#start')
+const watchBlockStop = document.querySelector('#stop')
+const watchBlockReset = document.querySelector('#reset')
+
+let interval
+
+let minutes = 0;
+let second = 0;
+let milliseconds = 0;
+
+const startTimer = () => {
+    milliseconds++
+
+
+    if (milliseconds <= 99) {
+        watchHundSecond.innerHTML = milliseconds;
+    }
+    if (milliseconds === 100) {
+        watchHundSecond.innerHTML = '00';
+    }
+
+    if (milliseconds > 99) {
+        second++
+        watchSecond.innerHTML = '0' + second
+
+        milliseconds = 0
+    }
+    if (second > 9) {
+        watchSecond.innerHTML = second
+
+    }
+    if (second > 59) {
+        minutes++
+        watchBlock.innerHTML = '0' + minutes
+
+        second = 0
+        watchSecond.innerHTML = '0' + second
+
+    }
+    if (minutes > 9) {
+        watchBlock.innerHTML = minutes
+    }
+
+
+};
+watchBlockStart.addEventListener('click', () => {
+    clearInterval(interval)
+    interval = setInterval(startTimer, 10);
+});
+watchBlockStop.addEventListener('click', () => {
+    clearInterval(interval)
+});
+watchBlockReset.addEventListener('click', () => {
+    clearInterval(interval)
+    minutes = 0
+    second = 0
+    milliseconds = 0
+
+    watchBlock.innerHTML = '00'
+    watchSecond.innerHTML = '00'
+    watchHundSecond.innerHTML = '00'
+
+});
